7bed3f789741732d41184ea9d49d8a657931411e
Nikolay Lukhtura <rrwniko@gmail.com>
2022-06-16 18:45:32 +0200

    fixed startInterval()

--- js/script.js
+++ js/script.js
@@ -3,24 +3,14 @@ const prevButton = document.querySelector('.prev-button');
 const sliderLine = document.querySelector('.slider-line');
 prevButton.disabled = true;
 let offset = 0;
-let count = 1;
+let sliderCount = 1;
 let intervalID = setInterval(toNextSLide, 5000);
-
-function startInterval() {
-    intervalID = setInterval(toNextSLide, 5000);
-};
-
-function stopInterval() {
-    clearInterval(intervalID);
-};
 
 nextButton.addEventListener('click', () => {
     toNextSLide();
     checkButtonAndInterval();
     stopInterval();
-    if (count < 9) {
-        startInterval();
-    };
+    startInterval();
 });
 
 prevButton.addEventListener('click', () => {
@@ -30,29 +20,39 @@ prevButton.addEventListener('click', () => {
     startInterval();
 });
 
+function startInterval() {
+    if (sliderCount < 9) {
+        intervalID = setInterval(toNextSLide, 5000);
+    };
+};
+
+function stopInterval() {
+    clearInterval(intervalID);
+};
+
 function toNextSLide() {
-    count += 1;
+    sliderCount += 1;
     checkButtonAndInterval();
     offset += 400;
     sliderLine.style.left = `${-offset}px`;
 };
 
 function toPreviousSlide() {
-    count--;
+    sliderCount--;
     offset -= 400;
     sliderLine.style.left = `${-offset}px`;
 };
 
 function checkButtonAndInterval() {
-    if (count === 1) {
+    if (sliderCount === 1) {
         prevButton.disabled = true
     } else {
         prevButton.disabled = false
     };
-    if (count > 8) {
+    if (sliderCount > 8) {
         stopInterval();
         nextButton.disabled = true
-    } else if (count !== 9) {
+    } else if (sliderCount !== 9) {
         nextButton.disabled = false
     };
 };
